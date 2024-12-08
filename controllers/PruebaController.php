113edf1d8f284a121b75b526ddba52f460083421
Lemends24 <jesusgregorio307@gmail.com>
2024-12-08 13:48:56 -0500

    Agregue un controlador prueba

---
+++ controllers/PruebaController.php
@@ -0,0 +1,10 @@
+<?php
+    namespace app\controllers;
+    use Yii;
+    use yii\web\Controller;
+    class PruebaController extends Controller {
+        public function actionHola(){
+            echo "voy bien";
+        }
+    }
+?>
