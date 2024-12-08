e1b186cfc6c900ffaa476ba632dd050bca07c4f0
Lemends24 <jesusgregorio307@gmail.com>
2024-12-08 15:55:22 -0500

    Create PagosController.php

---
+++ controllers/PagosController.php
@@ -0,0 +1,10 @@
+<?php
+  namespace app\controllers;
+  use Yii;
+  use yii\web\controller;
+  class PagosController extends controller{
+    public function actionList(){
+      echo "Lista Pagos";
+    }
+  }
+?>
